0906b47b807c3825316dcab1765e21dc6deaa83e
Nevillealee <nevillealee@gmail.com>
2018-04-23 17:02:19 -0700

    minitest functionality added
    ready for more involved tests

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ Dotenv.load
 require 'active_record'
 require 'httparty'
 require "sinatra/activerecord"
-require_relative 'models/config'
+require_relative 'models/recharge_subs_config'
+require 'minitest/autorun'
 
-module TestRecharge
+module RechargeTest
   class User_subs
     def initialize
     Dotenv.load
@@ -13,12 +14,66 @@ module TestRecharge
     @my_header = {
         "X-Recharge-Access-Token" => @recharge_access_token
     }
-    @my_change_charge_header = {
-        "X-Recharge-Access-Token" => @recharge_access_token,
-        "Accept" => "application/json",
-        "Content-Type" =>"application/json"
-    }
+    end
+    # pass in hash with subscription id (have another rake task gen list of ids?)
+    def pull(params)
+      @response = HTTParty.get("https://api.rechargeapps.com/subscriptions/#{params['subscription_id']}", :headers => @my_header)
+      if @response.code == 200
+        return @response.body
+      else
+        # LOG ERROR TO OUTPUT
+        return @response
+      end
+    end
+  end
+
+  class Mini < Minitest::Test
+    def test_request_subscription
+      user_sub = User_subs.new
+      params = {subscription_id: '507621212192'}
+      sub_properties = user_sub.pull(params)
+      assert sub_properties == 
     end
   end
 
 end
+
+
+#  EXAMPLE RESPONSE
+# {
+#     "subscription": {
+#         "id": 13771142,
+#         "address_id": 9364792,
+#         "customer_id": 9476622,
+#         "created_at": "2018-03-27T14:26:12",
+#         "updated_at": "2018-03-27 14:26:12",
+#         "next_charge_scheduled_at": "2017-04-01T00:00:00",
+#         "cancelled_at": null,
+#         "product_title": "Sumatra Coffee",
+#         "variant_title": "Milk - a / b",
+#         "price": 12,
+#         "quantity": 1,
+#         "status": "ACTIVE",
+#         "shopify_product_id": 1255183683,
+#         "shopify_variant_id": 3844924611,
+#         "sku": null,
+#         "order_interval_unit": "day",
+#         "order_interval_frequency": "30",
+#         "charge_interval_frequency": "30",
+#         "cancellation_reason": null,
+#         "cancellation_reason_comments": null,
+#         "order_day_of_week": null,
+#         "order_day_of_month": null,
+#         "properties": [
+#             {
+#                 "name": "grind",
+#                 "value": "drip"
+#             },
+#             {
+#                 "name": "size",
+#                 "value": "medium"
+#             }
+#         ],
+#         "expire_after_specific_number_of_charges": 2
+#     }
+# }
--- models/recharge_subs_config.rb
+++ models/recharge_subs_config.rb
@@ -1,6 +1,6 @@
 class RechargeSubsConfig < ActiveRecord::Base
   self.table_name = "recharge_subs_config"
-
+  # CREATE OBJECT TO TEST AGAINST RECHARGE RESPONSE
   attr_accessor :product_id, :variant_id, :sku, :product_title, :collection_property
   def initialize(prod_id, variant_id, sku, prod_title, collection_prop)
     @product_id = prod_id
